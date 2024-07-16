ccd26c6b23f6d5f93fdaae0cf55d60e3bfedc137
Salman Bin Kashif <salmanbinkashif@gmail.com>
2024-07-16 17:20:40 +0000

    do list exercise

--- 02/02_02/02_02e/main.py
+++ 02/02_02/02_02e/main.py
@@ -1,11 +1,17 @@
-''' 
+""" 
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
-'''
+"""
 
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
-NUM_OF_STUDENTS = len(student_pet_count_list)
-print(NUM_OF_STUDENTS)
+print(student_pet_count_list)
 
-# average = sum / number of items
+NUM_OF_STUDENTS = len(student_pet_count_list)
+
+print("Number of students in the class: ", NUM_OF_STUDENTS)
+
+SUM=0
+for INDIDVIDUAL_PET_COUNT in student_pet_count_list:
+    SUM=SUM+INDIDVIDUAL_PET_COUNT
+print(SUM)
